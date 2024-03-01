7fff09a3063b5c237901974607768cb72f179618
Franco Valdés <franco.v.navarro@gmail.com>
2024-03-01 15:04:22 -0300

    Configured wsgi.py

--- chemically/chemically/wsgi.py
+++ chemically/chemically/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
+path = os.path.expanduser("~/chemic-ally/chemically")
+if path not in sys.path:
+    path.insert(0, path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chemically.settings')
-
 application = get_wsgi_application()
