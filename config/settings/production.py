81cd6a3aee85cbac8892b53aa9211043005239c9
Franco <franco.v.navarro@gmail.com>
2026-05-02 20:07:12 -0400

    Added Standard Output to production settings for better integration with AWS EB

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,3 +15,25 @@ DATABASES = {
         "CONN_MAX_AGE": 600,
     }
 }
+
+# Log to Standard Output (stdout) for better integration with Elastic Beanstalk's logging system
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler', # Switches from FileHandler to Console
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
