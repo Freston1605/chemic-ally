5fe5ee15d6a2d71ad793d2a110a8b9ee037fbc71
Franco <franco.v.navarro@gmail.com>
2026-05-06 12:38:24 -0400

    Moved default static file configuration from the config.settings.base to config.settings.deevelopment and added AWS S3 setting for serving static files into the config.setting.development config file.

--- config/settings/development.py
+++ config/settings/development.py
@@ -14,3 +14,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 # Set to True for local development to avoid slow static file lookups
 WHITENOISE_AUTOREFRESH = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- config/settings/production.py
+++ config/settings/production.py
@@ -22,3 +22,18 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "https://chemically-env.eba-pyxp2kzs.us-east-2.elasticbeanstalk.com",
 ]
+
+
+# Static files (CSS, JavaScript, Images)
+# AWS S3 setting
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+# s3 static settings
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
